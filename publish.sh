892e77765f9de4a5064284e9c8b1dfbb6d1dde8d
Mikaël Barbero <mikael.barbero@gmail.com>
2020-01-06 23:21:21 +0100

    Fix git push command

--- publish.sh
+++ publish.sh
@@ -32,4 +32,5 @@ echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
 echo "Pushing to github"
-git push origin gh-pages:gh-pages
+auth_header="$(git config --local --get http.https://github.com/.extraheader)"
+git push -c "http.extraheader=${auth_header}" origin gh-pages:gh-pages
