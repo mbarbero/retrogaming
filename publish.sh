390029be13a59f80fec3f00c13a9ab4c4dee5a93
Mikaël Barbero <mikael.barbero@gmail.com>
2020-01-06 22:54:15 +0100

    Download Hudo in publish script

--- publish.sh
+++ publish.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+HUGO_VERSION="0.62.2"
+
 if [ "$(git status -s)" ]
 then
     echo "The working directory is dirty. Please commit any pending changes."
@@ -18,8 +20,11 @@ git worktree add -B gh-pages public origin/gh-pages
 echo "Removing existing files"
 rm -rf public/*
 
+curl -sSL "https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz" > /tmp/hugo.tar.gz && tar -f /tmp/hugo.tar.gz -xz
+
+
 echo "Generating site"
-hugo --theme book
+./hugo --theme book
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
